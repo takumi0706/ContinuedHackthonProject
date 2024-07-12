7423162f6f8e01cc472f9b9409618f08a1c75d10
takumi <ganndamu0706@gmail.com>
2024-07-12 21:50:42 +0900

    feat:add contact of controller

---
+++ springboot/src/main/java/com/develop/TravelTango/controller/ContactController.java
@@ -0,0 +1,14 @@
+package com.develop.TravelTango.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ContactController {
+
+    @GetMapping("/contact")
+    public String contact() {
+
+        return "Contact";
+    }
+}
